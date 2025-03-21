bbdf9541679ae906226c1aa6c54a8353605b396d
I-Miteshhh <imiteshhh@gmail.com>
2025-03-21 23:32:21 +0530

    Add LC-28 in StringOperations

--- Algorithms/Strings/StringOperations/firstOccurrence.java
+++ Algorithms/Strings/StringOperations/firstOccurrence.java
@@ -7,6 +7,8 @@ Input: haystack = "sadbutsad", needle = "sad"
 Output: 0
 Explanation: "sad" occurs at index 0 and 6.
 The first occurrence is at index 0, so we return 0.
+
+Use of Stringoperations is allowed.
  */
 class Solution {
     public int strStr(String haystack, String needle) {
